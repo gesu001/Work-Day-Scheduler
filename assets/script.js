4108accf22a2391794236c1b83c0495d667560cb
Ge Su <gegege.su@gmail.com>
2023-04-20 00:59:13 +1000

    update backgroundColor function in js

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,51 @@
-
 $(function () {
-
   var timeDisplayEl = $('#currentDay')
   var btnEl = $('i');
-
-function displayTime() {
-var currentDay = dayjs().format('dddd, MMMM DD');
-timeDisplayEl.text(currentDay);
-}
-
-function backgroundColor() {
-  var currentHour = dayjs().format('H'); 
-  var rows = document.getElementsByClassName("row");
-  var rows =Array.from(rows).forEach(function(row) {
-    var rowHour = row.id.split("_")[1];
-
-    if (rowHour) {
-      if (rowHour === currentHour) {
-        addClass(row, "present");
-      } else if (rowHour > currentHour) {
-        addClass(row, "future");
-      } else if (rowHour < currentHour) {
-        addClass(row, "past");
-      } 
+  
+  function displayTime() {
+  var currentDay = dayjs().format('dddd, MMMM DD');
+  timeDisplayEl.text(currentDay);
   }
-  function addClass(element, classname) {
-    element.classList.add(classname);
+  
+  function backgroundColor () {
+    var rows = document.getElementsByClassName("row");
+    var currentHour = parseInt(dayjs().format('H')); 
+    
+    Array.from(rows).forEach(row => {
+        var rowId = row.id;
+        var rowHour = parseInt(rowId.split("_")[1]);
+        
+        if (currentHour === rowHour) {
+          row.classList.add("present") 
+        } else if (currentHour < rowHour) {
+          row.classList.add("future")
+        } else if (currentHour > rowHour) {
+          row.classList.add("past")
+        }
+    })
   }
-})
-}
-
-function handleTaskSubmit(event){
-  event.preventDefault();
-  var tasktime = $(this).parent().parent().attr('id')
-  var taskContent  = $(this).parent().prev().val().trim()
-  if (!taskContent){
-    return;
+  
+  function handleTaskSubmit(event){
+    event.preventDefault();
+    var tasktime = $(this).parent().parent().attr('id')
+    var taskContent  = $(this).parent().prev().val().trim()
+    if (!taskContent){
+      return;
+    }
+    localStorage.setItem(tasktime, taskContent);
   }
-  localStorage.setItem(tasktime, taskContent);
-
-}
-
-function readTaskFromStorage() {
-  var tasktime = $(this).parent().parent().attr('id')
-  var task = localStorage.getItem(tasktime);
-  $(this).parent().prev().val(task)
-  if (!task) {
-    return
-  } 
-}
-
-readTaskFromStorage();
-
-displayTime();
-backgroundColor();
-
-btnEl.on('click', handleTaskSubmit);
-
-})
+  
+  function readTaskFromStorage() {
+    var tasktime = $(this).parent().parent().attr('id')
+    var task = localStorage.getItem(tasktime);
+    $(this).parent().prev().val(task)
+    if (!task) {
+      return
+    } 
+  }
+  readTaskFromStorage();
+  displayTime();
+  backgroundColor ()
+  btnEl.on('click', handleTaskSubmit);
+  })
+  
